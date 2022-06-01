6db9a98b5c28307964bf305117978d523c603ab4
SimonBox <simon@resim.ai>
2022-06-01 12:57:14 -0700

    Add specific large and small tangent vectors to the test cases. (#50)
    ## Description of change
    Add to the list of standard test case vectors specific cases for small and 
    large vectors. This is important for hitting all the code paths in se3.cc.
    Some of the methods in se3 have explicit control flow for small and large 
    vectors.
    ## Asana task link
    [Asana task](https://app.asana.com/0/0/1202363813071891/f)
    ## Guide to reproduce test results.
    ```
    bazel test //transforms:liegroups_test_helpers_test
    bazel test //transforms:liegroups_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

--- transforms/liegroup_test_helpers.cc
+++ transforms/liegroup_test_helpers.cc
@@ -7,11 +7,44 @@
 #include "transforms/se3.hh"
 #include "transforms/so3.hh"
 
+namespace {
+
+using SO3 = resim::transforms::SO3;
+using SE3 = resim::transforms::SE3;
+
+template <typename Vector>
+Vector make_large_vector() {
+  constexpr double LARGE = 1E6;
+  return Vector::Random() * LARGE;
+}
+
+template <>
+SO3::TangentVector make_large_vector<SO3::TangentVector>() {
+  // Close to Pi
+  constexpr double NEARLY_PI = M_PI - 0.01;
+  constexpr double SQRT_3 = 1.732;
+  constexpr double LRG_ROT = NEARLY_PI / SQRT_3;
+  return SO3::TangentVector{LRG_ROT, LRG_ROT, -LRG_ROT};
+}
+
+template <>
+SE3::TangentVector make_large_vector<SE3::TangentVector>() {
+  return SE3::tangent_vector_from_parts(
+      make_large_vector<SO3::TangentVector>(),
+      make_large_vector<Eigen::Vector3d>());
+}
+
+}  // namespace
+
 namespace resim {
 namespace transforms {
 
 template <typename Vector>
 std::vector<Vector> make_test_vectors() {
+  // Make random seed determistic
+  constexpr unsigned int SEED = 42;
+  srand(SEED);
+  // How many test elements to make.
   constexpr int TEST_ELEMENT_COUNT = 7;
   std::vector<Vector> elements;
   // Add a zero element.
@@ -20,9 +53,12 @@ std::vector<Vector> make_test_vectors() {
   elements.push_back(Vector::Ones());
   // Add a negative ones element.
   elements.push_back(-Vector::Ones());
+  // Add a large element.
+  elements.push_back(make_large_vector<Vector>());
+  // Add a tiny numbers element.
+  constexpr double TINY = 1E-6;
+  elements.push_back(Vector::Random() * TINY);
   // Populate the remainder with random elements.
-  constexpr unsigned int SEED = 42;
-  srand(SEED);
   for (int i = elements.size(); i < TEST_ELEMENT_COUNT; ++i) {
     elements.push_back(Vector::Random());
   }
