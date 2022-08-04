84d84e4c854cbffe9ed4ade6ede19a13191fb544
Michael Bauer <michael@resim.ai>
2022-08-04 09:32:25 -0700

    Add InOut wrapper for references (#98)
    ## Description of change
    Add a wrapper for references that makes signatures for functions that modify their arguments more explicit.
    
    ## Asana task link
    None.
    ## Guide to reproduce test results.
    ```bash
    bazel test //common/...
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ common/inout.hh
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace resim {
+
+// This class is a simple wrapper around a reference that one can use in
+// function signatures to make it impossible for arguments to be modified
+// without clients expecting it. In other words, when passing an argument to a
+// function taking an InOut<T>, a user must wrap their object in an InOut first
+// making it obvious that that object may be modified. For example compare the
+// two approaches:
+//
+// void foo(int &value) { ++value; }
+// void bar(InOut<int> value) { ++(*value); }
+//
+// // Client code:
+// int a = 5;
+// foo(a);  // a is incremented without a user having any indication that this
+//          // could happen
+//
+// bar(InOut{a}); // User can tell that a may be modified by bar().
+template <class T>
+class InOut {
+ public:
+  // Constructor from a reference to T
+  explicit InOut(T &x) : x_{x} {}
+
+  // Dereference the InOut
+  T &operator*() { return x_; }
+
+  // Access T's methods/members more conveniently
+  T *operator->() { return &x_; }
+
+ private:
+  explicit InOut(T *x);
+  T &x_;
+};
+
+}  // namespace resim
