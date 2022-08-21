ee5607fe242a72c81139eab501221eebdaf8d162
SimonBox <simon@resim.ai>
2022-08-20 19:31:59 -0700

    Quintic Polynomical Coefficients helper (#100)
    ## Description of change
    Compute the polynomial coefficients for a quintic Hermite spline.
    Polynomial coefficients calculation is straightforward, but requires a few
    magic numbers. This is unavoidable, but to save the reader of the Hermite spline
    class from wading through this clutter, we are abstracting these out to a helper
    class that can be called to serve up the required coefficients.
    
    We can test this class for correctness against hand computed coefficients, which
    also helps build up incremental testing of the components of the Hermite spline
    ## Asana task link
    [Asana task](https://app.asana.com/0/0/1202825153541987/f)
    ## Guide to reproduce test results.
    ```
    bazel test //curves:quintic_poly_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ curves/quintic_poly_coeffs.hh
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+namespace resim::curves {
+
+// Holds the polynomial coefficients for a TwoJet object.
+// The coefficients should by applied by pre-multiplication in tangent
+// space. This is trivial for the derivatives (e.g. a1 * d_frame_from_ref).
+// In the case of the LieGroup the coefficient is applied as:
+// Group::exp(a0 * frame_from_ref.log()); (as linear interpolation).
+struct TwoJetPolyCoeffs {
+  double a;    // for frame_from_ref()
+  double da;   // for d_frame_from_ref()
+  double d2a;  // for d2_frame_from_ref()
+};
+
+// Utility class to serve the quintic polynomial coefficients that are required
+// for a quintic Hermite curve segment.
+// Important assumptions:
+// Curve segments are defined by their boundary conditions at a pair of TwoJet
+// objects. These are called - respectively - the origin (orig) and the
+// destination (dest). Time is normalized over the segment, so n_time = {0, 1},
+// for points between the origin (n_time = 0) and destination (n_time = 1).
+class QuinticPolyCoeffs {
+ public:
+  // Constructing the quintic polynomial requires building and composing TwoJet
+  // objects for each of the algebra elements representing the transform between
+  // the origin and the destination and the first and second normalized-time
+  // derivatives at both the origin and the destination. The methods below fetch
+  // the TwoJetPolyCoeffs for each of these, given the value of normalized time
+  // (n_time).
+  static TwoJetPolyCoeffs dest_from_orig(double n_time);
+  static TwoJetPolyCoeffs d_orig(double n_time);
+  static TwoJetPolyCoeffs d_dest(double n_time);
+  static TwoJetPolyCoeffs d2_orig(double n_time);
+  static TwoJetPolyCoeffs d2_dest(double n_time);
+
+ private:
+  static constexpr unsigned int QUINTIC = 5;
+  // Each coefficient 'a'  is calulated as
+  // coeffs * {t, t^2, t^3, t^4, t^5}^T
+  // If the coefficient is for a first of second time derivative (da, d2a),
+  // then appropriate multiples (d_mult) are applied. See the comments against
+  // the data members below for more detail. In the implementation, the
+  // polynomial is evaluated using Horner's method.
+  static double two_jet_single_coeff(
+      const std::array<double, QUINTIC> &d_mult,
+      const std::array<double, QUINTIC> &coeffs,
+      double n_time,
+      int smallest_index);
+
+  // Compute coefficient a, da and d2a.
+  static TwoJetPolyCoeffs two_jet_coeffs(
+      const std::array<double, QUINTIC> &coeffs,
+      double n_time);
+
+  // Store the constants required to compute the coefficients.
+  // A coefficient 'a' is calculated from and array 'c' below as:
+  // a = c[0]t + c[1]t^2 ... +c[4]t^5
+  static constexpr std::array<double, QUINTIC> DEST_ORIG{0., 0., 10., -15., 6.};
+  static constexpr std::array<double, QUINTIC> D_ORIG{1., 0., -6., 8., -3.};
+  static constexpr std::array<double, QUINTIC> D_DEST{0., 0., -4., 7., -3.};
+  static constexpr std::array<double, QUINTIC> D2_ORIG{0., .5, -1.5, 1.5, -.5};
+  static constexpr std::array<double, QUINTIC> D2_DEST{0., 0., .5, -1., .5};
+
+  // To compute coefficients for the first and second time derivatives we need
+  // to differentiate the expressions built using the coefficients above i.e:
+  // a = c[0]t + c[1]t^2 ... + c[4]t^5 goes to:
+  // da = c[0] + 2c[1]t ... 5c[4]t^4 and then to.
+  // d2a = 2c[1] ... 20c[4]t^3.
+  // The multiples being applied to the coefficients here are captured below.
+  static constexpr std::array<double, QUINTIC> A{1, 1, 1, 1, 1};
+  static constexpr std::array<double, QUINTIC> DA{1, 2, 3, 4, 5};
+  static constexpr std::array<double, QUINTIC> D2A{0, 2, 6, 12, 20};
+};
+
+}  // namespace resim::curves
