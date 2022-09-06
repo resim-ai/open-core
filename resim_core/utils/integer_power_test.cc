78d4b081eb893edc05fd1e6a861223db821abab6
Michael Bauer <michael@resim.ai>
2022-09-06 15:48:56 -0700

    Add an integer power function (#116)
    ## Description of change
    Add a simple integer power function which uses repeated squaring to take powers when the given arguments are integers.
    
    Next PR: #117
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/0/1202868081746386/f)
    ## Guide to reproduce test results.
    ```bash
    bazel test //resim_core/utils:integer_power_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/utils/integer_power_test.cc
@@ -0,0 +1,48 @@
+#include "resim_core/utils/integer_power.hh"
+
+#include <cmath>
+#include <cstdint>
+#include <limits>
+
+#include "gtest/gtest.h"
+
+namespace resim {
+
+template <typename UInt_t>
+class IntegerPowerTest : public ::testing::Test {};
+
+template <typename UInt_t>
+class IntegerPowerDeathTest : public ::testing::Test {};
+
+using UIntTypes = ::testing::Types<uint8_t, uint16_t, uint32_t, uint64_t>;
+TYPED_TEST_SUITE(IntegerPowerTest, UIntTypes);
+TYPED_TEST_SUITE(IntegerPowerDeathTest, UIntTypes);
+
+TYPED_TEST(IntegerPowerTest, TestIntegerPowers) {
+  constexpr TypeParam MAX_BASE = 5U;
+  constexpr TypeParam MAX_EXPONENT = 5U;
+  for (TypeParam base = 1U; base < MAX_BASE; ++base) {
+    for (TypeParam exponent = 0U; exponent < MAX_EXPONENT; ++exponent) {
+      if (std::pow(base, exponent) <
+          static_cast<double>(std::numeric_limits<TypeParam>::max())) {
+        EXPECT_EQ(pow(base, exponent), std::pow(base, exponent));
+      }
+    }
+  }
+}
+
+TYPED_TEST(IntegerPowerTest, TestZeroToZero) { EXPECT_EQ(pow(0U, 0U), 1U); }
+
+TYPED_TEST(IntegerPowerDeathTest, TestOverFlow) {
+  // SETUP
+  constexpr TypeParam TWO{2U};
+  constexpr TypeParam WIDTH{std::numeric_limits<TypeParam>::digits};
+
+  // ACTION / VERIFICATION
+  // Should work fine:
+  pow(TWO, static_cast<TypeParam>(WIDTH - 1U));
+  // Should cause death:
+  EXPECT_DEATH(pow(TWO, WIDTH), "Overflow detected!");
+}
+
+}  // namespace resim
