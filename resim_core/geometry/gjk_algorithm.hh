81f7ba8f4fb719776efa7831358f30558898c6d5
Michael Bauer <michael@resim.ai>
2022-09-09 14:34:39 -0700

    Add GJK Algorithm (#121)
    ## Description of change
    Add the GJK Algorithm based on [this paper](https://drive.google.com/file/d/1A8j4b-Wknf6eAz8zh4YMKNLowfTuxXWD/view) by the original authors.
    
    Prev PRs: #117, #118
    Next PR: #122
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1202678889880094/1202879621033803/f)
    ## Guide to reproduce test results.
    ```bash
     bazel test //resim_core/transforms:gjk_algorithm_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/geometry/gjk_algorithm.hh
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <Eigen/Dense>
+#include <functional>
+#include <optional>
+
+namespace resim::geometry {
+
+template <int DIM>
+using SupportFunction = std::function<Eigen::Matrix<double, DIM, 1>(
+    const Eigen::Matrix<double, DIM, 1> &)>;
+
+constexpr int DEFAULT_MAX_ITERATIONS = 100;
+
+// Use the GJK algorithm to determine the minimum distance between the convex
+// sets described by support function 1 and support function 2.
+// @param support_1 - The support for object 1.
+// @param support_2 - The support for object 2.
+// @param max_iterations - The max number of iterations to allow for
+//                         convergence.
+// @returns The minimum distance between the sets, or a nullopt otherwise.
+template <int DIM>
+std::optional<double> gjk_algorithm(
+    const SupportFunction<DIM> &support_1,
+    const SupportFunction<DIM> &support_2,
+    int max_iterations = DEFAULT_MAX_ITERATIONS);
+
+}  // namespace resim::geometry
