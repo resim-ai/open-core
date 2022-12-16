f80d7c0f99afb01ff35abee6c58d068a9f6bccda
Michael Bauer <michael@resim.ai>
2022-12-16 09:33:36 -0800

    Add some helper functions to Timestamp (#164)
    ## Description of change
    Add some helper functions to make working with timestamps easier.
    ## Asana task link
    [Asana task](https://app.asana.com/0/1202622045346316/1203558253867220)
    ## Guide to reproduce test results.
    ```bash
    full_test () {
        TARGET="$1"
        VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
    
        bazel test --runs_per_test=100 "${TARGET}" && \
        bazel test --test_timeout=3600 --run_under="${VALGRIND}" "${TARGET}"
    }
    full_test //resim_core/time:timestamp_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

--- resim_core/time/timestamp.hh
+++ resim_core/time/timestamp.hh
@@ -7,10 +7,45 @@
 #pragma once
 
 #include <chrono>
+#include <cstdint>
+#include <ratio>
 
 namespace resim::time {
 
 using Duration = std::chrono::nanoseconds;
 using Timestamp = std::chrono::sys_time<Duration>;
+
+// Convert a duration to seconds as a double
+constexpr double as_seconds(const Duration duration) {
+  return std::chrono::duration<double>(duration).count();
+}
+
+// Create a duration from seconds as a double
+constexpr Duration as_duration(const double t_s) {
+  return std::chrono::duration_cast<Duration>(
+      std::chrono::duration<double>(t_s));
+}
+
+// A timestamp represented as integer seconds and nanoseconds. This
+// representation is common in robotics frameworks (ROS) and elsewhere
+// (e.g.
+// https://developers.google.com/protocol-buffers/docs/reference/google.protobuf#timestamp)
+struct SecsAndNanos {
+  int64_t secs = 0;
+  int32_t nanos = 0;
+};
+
+// Convert a duration to SecsAndNanos
+constexpr SecsAndNanos to_seconds_and_nanos(const Duration duration) {
+  return SecsAndNanos{
+      .secs = duration.count() / std::nano::den,
+      .nanos = static_cast<int32_t>(duration.count() % std::nano::den),
+  };
+}
+
+// Convert SecsAndNanos to a duration
+constexpr Duration from_seconds_and_nanos(const SecsAndNanos &secs_and_nanos) {
+  return Duration{secs_and_nanos.secs * std::nano::den + secs_and_nanos.nanos};
+}
 
 }  // namespace resim::time
