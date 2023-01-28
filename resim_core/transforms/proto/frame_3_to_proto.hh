3b5953486a7986dd74b8fd6d68b9f8811d1c9ee5
SimonBox <simon@resim.ai>
2023-01-27 20:21:27 -0800

    Frame<3> to proto (#219)
    ## Description of change
    Pack Frame<3> objects into a representatice proro message.
    ## Asana task link
    [Asana task](https://app.asana.com/0/1203751014069901/1203841707026878/f)
    ## Guide to reproduce test results.
    ```
    bazel test //resim_core/transforms/proto:frame_3_to_proto
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/transforms/proto/frame_3_to_proto.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "resim_core/transforms/frame.hh"
+#include "resim_core/transforms/proto/frame_3.pb.h"
+
+namespace resim::transforms::proto {
+
+void pack(const transforms::Frame<3> &in, Frame_3 *out);
+
+transforms::Frame<3> unpack(const Frame_3 &in);
+
+};  // namespace resim::transforms::proto
---
+++ resim_core/transforms/proto/frame_3_to_proto_test.cc
@@ -0,0 +1,43 @@
+#include "resim_core/transforms/proto/frame_3_to_proto.hh"
+
+#include <gtest/gtest.h>
+
+#include "resim_core/transforms/frame.hh"
+#include "resim_core/transforms/proto/frame_3.pb.h"
+
+namespace resim::transforms {
+
+TEST(Frame3ToProtoTest, Pack) {
+  // SETUP
+  proto::Frame_3 msg;
+  const auto frame = Frame<3>::new_frame();
+
+  // ACTION
+  pack(frame, &msg);
+
+  // VERIFICATION
+  EXPECT_EQ(msg.id().data(), frame.id().to_string());
+}
+
+TEST(Frame3ToProtoTest, RoundTrip) {
+  // SETUP
+  proto::Frame_3 msg;
+  const auto frame = Frame<3>::new_frame();
+
+  // ACTION
+  pack(frame, &msg);
+  const auto retrieved_frame = unpack(msg);
+
+  // VERIFICATION
+  EXPECT_EQ(retrieved_frame.DIMS, 3);
+  EXPECT_EQ(retrieved_frame, frame);
+}
+
+TEST(Frame3ToProtoDeathTest, TestPackInvalid) {
+  // ACTION / VERIFICATION
+  EXPECT_DEATH(
+      proto::pack(Frame<3>::new_frame(), nullptr),
+      "Can't pack into invalid proto!");
+}
+
+}  // namespace resim::transforms
