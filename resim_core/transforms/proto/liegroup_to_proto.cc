38dc3e0eb1b851aa0b84aa51adc195999df1e7fe
SimonBox <simon@resim.ai>
2023-02-01 13:49:21 -0800

    Packers for Framed groups. (#226)
    ## Description of change
    Pack Framed groups like FSE3, FSO3 into matching proto messages.
    ## Asana task link
    [Asana task](https://app.asana.com/0/1203751014069901/1203869115126125/f)
    ## Guide to reproduce test results.
    ```
    bazel test //resim_core/transforms/proto:framed_group_to_proto_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

--- resim_core/transforms/proto/liegroup_to_proto.cc
+++ resim_core/transforms/proto/liegroup_to_proto.cc
@@ -3,10 +3,12 @@
 #include <glog/logging.h>
 
 #include "resim_core/math/proto/matrix_to_proto.hh"
+#include "resim_core/transforms/framed_group.hh"
 #include "resim_core/transforms/proto/se3.pb.h"
 #include "resim_core/transforms/proto/so3.pb.h"
 #include "resim_core/transforms/se3.hh"
 #include "resim_core/transforms/so3.hh"
+#include "resim_core/utils/inout.hh"
 
 namespace resim::transforms::proto {
 
@@ -26,6 +28,8 @@ void unpack_liegroup(const Msg &in, InOut<Group> out) {
 
 template void pack_liegroup(const transforms::SE3 &, SE3 *);
 template void pack_liegroup(const transforms::SO3 &, SO3 *);
+template void pack_liegroup(const transforms::FSE3 &, SE3 *);
+template void pack_liegroup(const transforms::FSO3 &, SO3 *);
 template void unpack_liegroup(const SE3 &, InOut<transforms::SE3>);
 template void unpack_liegroup(const SO3 &, InOut<transforms::SO3>);
 
