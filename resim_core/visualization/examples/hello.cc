2150eebdf8bcd8bf15c6bda9ba2eaeffdd35a877
Michael Bauer <michael@resim.ai>
2023-02-05 20:45:02 -0800

    Add a hello view script! (#239)
    ## Description of change
    Add a hello view script! It doesn't work yet since we haven't connected up the C++ client yet.
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1203751014069901/1203751016100578/f)
    ## Guide to reproduce test results.
    ```bash
    bazel run //resim_core/visualization/examples:hello
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [ ] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/visualization/examples/hello.cc
@@ -0,0 +1,35 @@
+
+#include <cstdlib>
+
+#include "resim_core/transforms/se3.hh"
+#include "resim_core/transforms/so3.hh"
+#include "resim_core/visualization/view.hh"
+
+int main(int argc, char *argv[]) {
+  using resim::transforms::SE3;  // ReSim's 6 d.o.f. rigid xform.
+  using resim::transforms::SO3;  // ReSim's 3 d.o.f. rotation.
+
+  // Create a simple rotation of pi/2 about the Z-axis.
+  const double ANGLE_RAD = M_PI_2;
+  const double AXIS_X = 0.0;
+  const double AXIS_Y = 0.0;
+  const double AXIS_Z = 1.0;
+
+  const double TRANSLATION_X = 1.0;
+  const double TRANSLATION_Y = 0.0;
+  const double TRANSLATION_Z = 1.0;
+
+  const SO3 world_from_robot_rot(ANGLE_RAD, {AXIS_X, AXIS_Y, AXIS_Z});
+
+  // Combine the rotation with a simple translation {x, y, z}
+  // to make an SE3 rigid transform.
+  const SE3 world_from_robot(
+      world_from_robot_rot,
+      {TRANSLATION_X, TRANSLATION_Y, TRANSLATION_Z});
+
+  // Visualize the resultant transform
+  // Run this line and view at app.resim.ai
+  resim::view << world_from_robot;
+
+  return EXIT_SUCCESS;
+}
