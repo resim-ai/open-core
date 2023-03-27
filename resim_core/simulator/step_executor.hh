63b7099816d5681928055dd49d95c97e2dc35040
Michael Bauer <michael@resim.ai>
2023-03-27 13:12:46 -0700

    Add Step Executor Interface and Implementation (#195)
    ## Description of change
    Add an interface for the step executor which handles the interactions between different SimulationUnits on each step.
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1202178773526279/1203746893882531/f)
    ## Guide to reproduce test results.
    ```bash
    full_test () {
        TARGET="$1"
        VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
    
        bazel test --runs_per_test=100 "${TARGET}" && \
        bazel test --test_timeout=3600 --run_under="${VALGRIND}" "${TARGET}"
    }
    full_test //resim_core/simulator:simple_step_executor_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/simulator/step_executor.hh
@@ -0,0 +1,51 @@
+
+#pragma once
+
+#include <functional>
+#include <optional>
+#include <string_view>
+
+namespace resim::simulator {
+
+// This library represents the interface for a class responsible for running a
+// set of given tasks while respecting order dependence on each step. In
+// particular, each `Task` is specified with a name, an optional dependency
+// string, a provision string, and a piece of work to execute. Here's how this
+// works. A given task may run if and only if all tasks whose provisions match
+// its dependency have previously run. If the dependency is `std::nullopt`, then
+// the task will run at an arbitrary time within the step, although any tasks
+// depending on its provision will run after it to comply with the previous
+// requirement. If the task's dependency is not provided by any other task, **it
+// will not run**. Furthermore, each step should run independently. For
+// instance, completion of a task on a previous step should not affect its
+// dependents on future steps. The work function returns a boolean reflecting
+// whether or not it was successful. This allows us to support rudimentary
+// branching logic (although the `false` branch just does nothing in this case),
+// which is convenient for our use case. This interface is written so that it
+// could work with a third-party parallel task scheduler like
+// [Taskflow](https://taskflow.github.io/taskflow/index.html) or Intel's
+// [Threading Building Blocks](https://github.com/oneapi-src/oneTBB).
+class StepExecutor {
+ public:
+  // A struct representing a task as described above.
+  struct Task {
+    std::string_view name;
+    std::optional<std::string_view> dependency;
+    std::string_view provision;
+
+    using Work = std::function<bool()>;
+    Work work;
+  };
+
+  StepExecutor() = default;
+  virtual ~StepExecutor() = default;
+  StepExecutor(const StepExecutor &) = default;
+  StepExecutor(StepExecutor &&) = default;
+  StepExecutor &operator=(const StepExecutor &) = default;
+  StepExecutor &operator=(StepExecutor &&) = default;
+
+  // Run the tasks for this step as described above.
+  virtual void run_step() = 0;
+};
+
+}  // namespace resim::simulator
