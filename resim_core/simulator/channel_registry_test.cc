335df90260b74bf29556be96d2184061d0e6aec1
Michael Bauer <michael@resim.ai>
2023-03-27 12:03:57 -0700

    Make a ChannelRegistry to help us setup channels (#192)
    ## Description of change
    Make a unified registry of channels responsible for creating and maintaining new channels for each topic we require communications on.
    
    Prev PR: #191
    ## Asana task link
    [Asana task](https://app.asana.com/0/1202178773526279/1203717703460957/f)
    ## Guide to reproduce test results.
    ```bash
    full_test () {
        TARGET="$1"
        VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
    
        bazel test --runs_per_test=100 "${TARGET}" && \
        bazel test --test_timeout=3600 --run_under="${VALGRIND}" "${TARGET}"
    }
    full_test //resim_core/simulator:channel_registry_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/simulator/channel_registry_test.cc
@@ -0,0 +1,127 @@
+
+#include "resim_core/simulator/channel_registry.hh"
+
+#include <gtest/gtest.h>
+
+#include "resim_core/assert/assert.hh"
+
+namespace resim::simulator {
+
+template <typename T>
+using ChannelPtr = std::shared_ptr<const Channel<T>>;
+
+template <typename T>
+using PublisherPtr = std::shared_ptr<const Publisher<T>>;
+
+template <typename T>
+class ChannelRegistryTest : public ::testing::Test {};
+using TestTypes = ::testing::Types<int, double>;
+TYPED_TEST_SUITE(ChannelRegistryTest, TestTypes);
+
+TYPED_TEST(ChannelRegistryTest, TestChannelGetter) {
+  // SETUP
+  ChannelRegistry registry;
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  constexpr std::string_view OTHER_TOPIC = "OTHER_TOPIC";
+
+  // ACTION
+  const ChannelPtr<TypeParam> channel_a{registry.channel<TypeParam>(TOPIC)};
+  const ChannelPtr<TypeParam> channel_b{registry.channel<TypeParam>(TOPIC)};
+  const ChannelPtr<TypeParam> channel_c{
+      registry.channel<TypeParam>(OTHER_TOPIC)};
+
+  // VERIFICATION
+  // We should get the same channel for the same topic.
+  EXPECT_EQ(channel_a, channel_b);
+
+  // We should get a different channel for a different topic.
+  EXPECT_NE(channel_a, channel_c);
+}
+
+TYPED_TEST(ChannelRegistryTest, TestReset) {
+  // SETUP
+  ChannelRegistry registry;
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+
+  // ACTION
+  const ChannelPtr<TypeParam> channel_a{registry.channel<TypeParam>(TOPIC)};
+  registry.reset();
+  const ChannelPtr<TypeParam> channel_b{registry.channel<TypeParam>(TOPIC)};
+
+  // VERIFICATION
+  // We should get a new channel for the same topic since we reset.
+  EXPECT_NE(channel_a, channel_b);
+}
+
+TYPED_TEST(ChannelRegistryTest, TestMakePublisher) {
+  // SETUP
+  ChannelRegistry registry;
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  const TypeParam MY_VALUE = 3;
+
+  const ChannelPtr<TypeParam> channel{registry.channel<TypeParam>(TOPIC)};
+
+  // ACTION
+  const PublisherPtr<TypeParam> publisher{
+      registry.make_publisher<TypeParam>(TOPIC)};
+
+  // VERIFICATION
+  // Make sure the publisher is connected to the channel.
+  publisher->publish(MY_VALUE);
+  EXPECT_EQ(channel->data().size(), 1U);
+  EXPECT_EQ(channel->data().front(), MY_VALUE);
+}
+
+TEST(ChannelRegistryDeathTest, TestChannelChannelBadType) {
+  // SETUP
+  ChannelRegistry registry;
+
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  using OneType = int;
+  using AnotherType = double;
+
+  // ACTION / VERIFICATION
+  registry.channel<OneType>(TOPIC);
+  EXPECT_THROW(registry.channel<AnotherType>(TOPIC), AssertException);
+}
+
+TEST(ChannelRegistryDeathTest, TestChannelPublisherBadType) {
+  // SETUP
+  ChannelRegistry registry;
+
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  using OneType = int;
+  using AnotherType = double;
+
+  // ACTION / VERIFICATION
+  registry.channel<OneType>(TOPIC);
+  EXPECT_THROW(registry.make_publisher<AnotherType>(TOPIC), AssertException);
+}
+
+TEST(ChannelRegistryDeathTest, TestPublisherChannelBadType) {
+  // SETUP
+  ChannelRegistry registry;
+
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  using OneType = int;
+  using AnotherType = double;
+
+  // ACTION / VERIFICATION
+  registry.make_publisher<OneType>(TOPIC);
+  EXPECT_THROW(registry.channel<AnotherType>(TOPIC), AssertException);
+}
+
+TEST(ChannelRegistryDeathTest, TestPublisherPublisherBadType) {
+  // SETUP
+  ChannelRegistry registry;
+
+  constexpr std::string_view TOPIC = "MY_TOPIC";
+  using OneType = int;
+  using AnotherType = double;
+
+  // ACTION / VERIFICATION
+  registry.make_publisher<OneType>(TOPIC);
+  EXPECT_THROW(registry.make_publisher<AnotherType>(TOPIC), AssertException);
+}
+
+}  // namespace resim::simulator
