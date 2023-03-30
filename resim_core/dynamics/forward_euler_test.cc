d0315f0554b1fcfe752232644c5608d9af7621e3
Michael Bauer <michael@resim.ai>
2023-03-30 15:25:15 -0700

    Add Forward Euler (#300)
    ## Description of change
    Add Forward Euler to core.
    
    Prev PR: #299
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1204081589980528/1204132592916153/f)
    ## Guide to reproduce test results.
    ```bash
    full_test () {
        TARGET="$1"
        VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
    
        bazel test --runs_per_test=100 "${TARGET}" && \
        bazel test --test_timeout=3600 --run_under="${VALGRIND}" "${TARGET}"
    }
    full_test //resim_core/dynamics:forward_euler_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ resim_core/dynamics/forward_euler_test.cc
@@ -0,0 +1,68 @@
+#include "resim_core/dynamics/forward_euler.hh"
+
+#include <gtest/gtest.h>
+
+#include <chrono>
+
+#include "resim_core/dynamics/controller.hh"
+#include "resim_core/dynamics/dynamics.hh"
+#include "resim_core/dynamics/testing/oscillator.hh"
+#include "resim_core/time/sample_interval.hh"
+
+namespace resim::dynamics {
+
+// Test that Forward Euler correctly integrates the oscillator system.
+TEST(ForwardEulerTest, TestForwardEuler) {
+  // SETUP
+  constexpr double STIFFNESS = 2.0;
+  constexpr double INITIAL_VELOCITY = 1.5;
+  const double omega_radps = std::sqrt(STIFFNESS);
+
+  const std::unique_ptr<Integrator<testing::OscillatorState>> integrator =
+      std::make_unique<ForwardEuler<testing::OscillatorState>>();
+  const std::unique_ptr<
+      Dynamics<testing::OscillatorState, testing::OSCILLATOR_CONTROL_DIM>>
+      dynamics = std::make_unique<testing::OscillatorDynamics>();
+
+  const std::unique_ptr<
+      Controller<testing::OscillatorState, testing::OSCILLATOR_CONTROL_DIM>>
+      controller = std::make_unique<testing::OscillatorController>(STIFFNESS);
+
+  testing::OscillatorState state{
+      .displacement = 0.,
+      .velocity = INITIAL_VELOCITY,
+  };
+
+  constexpr time::Duration MAX_DT{std::chrono::microseconds(10)};
+  constexpr time::Timestamp START_TIME;
+  const time::Timestamp end_time{START_TIME + std::chrono::seconds(1)};
+
+  // ACTION
+  // Integrate forward over a second using Forward Euler
+  time::Timestamp last_time = START_TIME;
+  time::sample_interval(
+      START_TIME,
+      end_time,
+      MAX_DT,
+      [&](const time::Timestamp t) {
+        // Do nothing on the first step
+        if (t == last_time) {
+          return;
+        }
+        const time::Duration dt{t - last_time};
+        state = (*integrator)(dt, state, t, *dynamics, *controller);
+
+        // VERIFICATION
+        // Analytical solution:
+        const double expected_displacement =
+            (INITIAL_VELOCITY / omega_radps) *
+            std::sin(omega_radps * time::as_seconds(t - START_TIME));
+
+        constexpr double TOLERANCE = 5e-5;
+        EXPECT_NEAR(state.displacement, expected_displacement, TOLERANCE);
+
+        last_time = t;
+      });
+}
+
+}  // namespace resim::dynamics
