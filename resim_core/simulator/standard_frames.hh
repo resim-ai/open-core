e448a7bead761df3c95c40b981e66c08209c2c9d
Michael Bauer <michael@resim.ai>
2023-05-02 15:14:04 -0700

    Add a SimulationUnit for Logging Visualization Data (#361)
    ## Description of change
    Add a LoggerUnit which can log FrameTransforms based on the received actor states.
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1204081589980528/1202791956573428/f)
    ## Guide to reproduce test results.
    ```bash
    full_test () {
        TARGET="$1"
        VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
    
        bazel test --runs_per_test=100 "${TARGET}" && \
        bazel test --test_timeout=3600 --run_under="${VALGRIND}" "${TARGET}"
    }
    full_test //resim_core/simulator:logger_unit_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

--- resim_core/simulator/standard_frames.hh
+++ resim_core/simulator/standard_frames.hh
@@ -9,7 +9,7 @@ namespace resim::simulator {
 
 // 'scenescenescenes' in ascii encoding:
 inline const transforms::Frame<3> SCENE_FRAME{
-    UUID{"{7363656e-6573-6365-6e65-7363656e6573}"}};
+    UUID{"7363656e-6573-6365-6e65-7363656e6573"}};
 
 constexpr std::string_view SCENE_FRAME_NAME = "scene";
 
