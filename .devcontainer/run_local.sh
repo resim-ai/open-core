b2f491fae175a9e30534aff94cb54eeb376670a1
Ubuntu <ubuntu@ip-172-31-18-171.us-west-1.compute.internal>
2023-08-08 22:48:16 +0000

    Allow renaming of open-core

--- .devcontainer/run_local.sh
+++ .devcontainer/run_local.sh
@@ -4,11 +4,13 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+DIRPATH="/workspaces/${PWD##*/}"
+
 docker run -it \
        --platform linux/amd64 \
        -p 8080:8080 \
        -p 443:443 \
-       --volume $(pwd):/workspaces/open-core \
+       --volume $(pwd):$DIRPATH \
        --volume root-home:/root \
        --volume /var/run/docker.sock:/var/run/docker.sock \
-       core-local:latest /bin/bash -c "cd /workspaces/open-core; $SHELL"
+       core-local:latest /bin/bash -c "cd $DIRPATH; $SHELL"
