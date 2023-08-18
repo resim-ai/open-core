c50f4666556c73c1c054e3e9db785ae19756d934
Michael Bauer <michael@resim.ai>
2023-08-18 16:34:15 -0700

    Add function to filter tuples (#19)
    ## Description of change
    Add a function which filters elements out of a tuple by a given
    condition on their types.
    
    ## Guide to reproduce test results.
    ```bash
    bazel test //resim/utils:tuple_utils_test
    ```
    
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in
    this PR.

--- resim/utils/tuple_utils.hh
+++ resim/utils/tuple_utils.hh
@@ -7,6 +7,9 @@
 #pragma once
 
 #include <tuple>
+#include <utility>
+
+#include "resim/utils/type.hh"
 
 namespace resim {
 
@@ -50,6 +53,104 @@ constexpr auto for_each_in_tuple(const Callable &f, Tuple &&t) {
         // clang-format on
       },
       std::forward<Tuple>(t));
+}
+
+// This constexpr helper function takes two index sequences and returns the
+// concatenation of the two.
+template <std::size_t... Idxs1, std::size_t... Idxs2>
+constexpr auto index_sequence_cat(
+    std::index_sequence<Idxs1...> /*unused*/,
+    std::index_sequence<Idxs2...> /*unused*/) {
+  return std::index_sequence<Idxs1..., Idxs2...>();
+}
+
+// This struct contains the implementation for the filter_tuple function below.
+// The goal of this function is to remove all entries from a given tuple where
+// the entry's type does not satisfy a given boolean type trait. For example,
+// running filter_tuple<std::is_integral>(std::make_tuple(1, "chair",
+// std::unique_ptr<int>())) should output a std::tuple<int> containing (1,).
+//
+// The implementation is here to perform the following steps:
+//
+// At compile time, the constexpr member function get_indices() uses tail-end
+// recursion to get the indices of the given tuple which satisfy the given
+// FilterTrait (i.e. FilterTrait<std::tuple_element_t<Index>>::value == true).
+// These are collected in an index sequence which is used with get_tuple() at
+// runtime in operator() to extract out the elements from the tuple with those
+// indices.
+//
+// This class uses a template template (not a typo) parameter of boolean type
+// trait we'd like to use for filtering. It determines for a given type
+// T whether that type satisfies a given constraint.
+template <template <typename T> typename FilterTrait>
+struct FilterTupleImpl {
+  // Base case
+  template <std::size_t IDX>
+  static constexpr auto get_indices() {
+    return std::index_sequence<>();
+  }
+
+  // Positive (should include) case.
+  template <
+      std::size_t IDX,
+      typename Head,
+      typename... Tail,
+      std::enable_if_t<FilterTrait<Head>::value, bool> = true>
+  static constexpr auto get_indices(
+      BasicType<Head> /*unused*/,
+      BasicType<Tail>... /*unused*/) {
+    // Make sure we include the current index.
+    return index_sequence_cat(
+        std::index_sequence<IDX>(),
+        get_indices<IDX + 1U>(TypeC<Tail>...));
+  }
+
+  // Negative (should discard) case.
+  template <
+      std::size_t IDX,
+      typename Head,
+      typename... Tail,
+      std::enable_if_t<not FilterTrait<Head>::value, bool> = true>
+  static constexpr auto get_indices(
+      BasicType<Head> /*unused*/,
+      BasicType<Tail>... /*unused*/) {
+    // Make sure we don't include the current index.
+    return get_indices<IDX + 1U>(TypeC<Tail>...);
+  }
+
+  // Convenient entrypoint for this function which immediately jumps into the
+  // recursion.
+  template <std::size_t IDX, typename... Ts>
+  static constexpr auto get_indices(const std::tuple<Ts...> & /*unused*/) {
+    return get_indices<IDX>(TypeC<Ts>...);
+  }
+
+  template <typename Tuple, std::size_t... Idxs>
+  static constexpr auto get_tuple(
+      Tuple &&t,
+      std::index_sequence<Idxs...> /*unused*/) {
+    return std::tuple<std::tuple_element_t<
+        Idxs,
+        std::remove_cv_t<std::remove_reference_t<Tuple>>>...>(
+        std::get<Idxs>(std::forward<Tuple>(t))...);
+  }
+
+  template <typename Tuple>
+  static constexpr auto filter(Tuple &&t) {
+    constexpr auto INDICES = decltype(get_indices<0U>(t))();
+    return get_tuple(std::forward<Tuple>(t), INDICES);
+  }
+};
+
+// Filter out the elements of a given tuple which satisfy FilterTrait. In other
+// words, remove elements for which FilterTrait<T>::value == false, leaving
+// behind a smaller tuple. The resulting tuple copies the elements of the input
+// tuple if it is an lvalue, and moves the elements if it is an rvalue. It must
+// therefore have copyable elements if it is an lvalue. For instance,
+// a std::tuple<int &&> lvalue cannot be filtered.
+template <template <typename T> typename FilterTrait, typename Tuple>
+constexpr auto filter_tuple(Tuple &&tuple) {
+  return FilterTupleImpl<FilterTrait>::filter(std::forward<Tuple>(tuple));
 }
 
 }  // namespace resim
