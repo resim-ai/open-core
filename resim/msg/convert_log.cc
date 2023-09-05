a11367ceb646e3c91258e48b55eb15db96386dd2
Michael Bauer <michael@resim.ai>
2023-09-05 14:59:29 -0700

    Add log converter script (#32)
    ## Description of change
    Add a script for the ROS2 -> ReSim log converter.
    
    Prev PRs: 
     - #31
    
    ## Guide to reproduce test results.
    ```bash
    bazel run //resim/msg:convert_log -- \
        --log /path/to/ros2_log.mcap \
        --output /path/to/converted.mcap
    ```
    Also ran the above under valgrind.
    
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [ ] This change is covered by tests that are already landed, or in
    this PR.

---
+++ resim/msg/convert_log.cc
@@ -0,0 +1,48 @@
+// Copyright 2023 ReSim, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include <cstdlib>
+#include <cxxopts.hpp>
+#include <filesystem>
+#include <iostream>
+
+#include "resim/msg/resim_log_from_ros2.hh"
+
+namespace resim::msg {
+
+int convert_log(const int argc, char **const argv) {
+  cxxopts::Options options{"convert_log", "Convert ROS2 Logs to ReSim Format."};
+  constexpr auto DEFAULT_PLUGIN_PATH = "resim/msg/default_converter_plugin.so";
+  // clang-format off
+  options.add_options()
+    ("l,log", "Log location (required)", cxxopts::value<std::string>())
+    ("o,output", "Output location (required)", cxxopts::value<std::string>())
+    ("p,plugin", "Converter plugin location", cxxopts::value<std::string>()->default_value(DEFAULT_PLUGIN_PATH))
+    ("h,help", "Print usage")
+  ;
+  // clang-format on
+  auto options_result = options.parse(argc, argv);
+
+  if (options_result.count("help") > 0U or options_result.count("log") == 0U or
+      options_result.count("output") == 0U) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+
+  const std::filesystem::path input_path{
+      options_result["log"].as<std::string>()};
+  const std::filesystem::path output_path{
+      options_result["output"].as<std::string>()};
+  const std::filesystem::path plugin_path{
+      options_result["plugin"].as<std::string>()};
+
+  resim_log_from_ros2(plugin_path, input_path, output_path);
+  return EXIT_SUCCESS;
+}
+
+}  // namespace resim::msg
+
+int main(int argc, char **argv) { return resim::msg::convert_log(argc, argv); }
