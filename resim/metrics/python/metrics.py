5a3016e8278db2429010208cb953afe0089cca6d
knowlsie <tomobknowles@gmail.com>
2023-10-05 16:51:35 +0000

    Fix indentation

--- resim/metrics/python/metrics.py
+++ resim/metrics/python/metrics.py
@@ -12,133 +12,133 @@ import numpy as np
 
 @dataclass
 class Timestamp:
-  secs: int
-  nanos: int
+    secs: int
+    nanos: int
 
 @dataclass
 class DoubleFailureDefinition:
-  fails_above: float
-  fails_below: float
+    fails_above: float
+    fails_below: float
 
 # Overall metrics object
 
 class ResimMetrics:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def add_metrics_data(self, data: MetricsData) -> MetricsData:
-    # Only adds if type is valid for job metrics data - i.e. can be packed.
-    # Also adds index
-    pass
+    def add_metrics_data(self, data: MetricsData) -> MetricsData:
+        # Only adds if type is valid for job metrics data - i.e. can be packed.
+        # Also adds index
+        pass
 
-  def add_metric(self, metric: Metric) -> Metric:
-    pass
+    def add_metric(self, metric: Metric) -> Metric:
+        pass
 
-  def pack(self) -> metrics_pb2.JobMetrics:
-    pass
+    def pack(self) -> metrics_pb2.JobMetrics:
+        pass
 
 # Metrics representation
 
 @dataclass(init=False, repr=True)
 class Metric:
-  id: uuid.UUID
-  name: str
-  description: str
-  parent_id: uuid.UUID
+    id: uuid.UUID
+    name: str
+    description: str
+    parent_id: uuid.UUID
 
-  status: MetricStatus
-  importance: MetricImportance
+    status: MetricStatus
+    importance: MetricImportance
 
-  should_display: Optional[bool]
-  blocking: Optional[bool]
+    should_display: Optional[bool]
+    blocking: Optional[bool]
 
 
 @dataclass(init=False, repr=True)
 class ScalarMetric(Metric):
-  value: float
+    value: float
 
-  failure_definition: Optional[DoubleFailureDefinition]
-  unit: Optional[str]
+    failure_definition: Optional[DoubleFailureDefinition]
+    unit: Optional[str]
 
-  def __init__(self,
-               name: str,
-               description: str,
-               parent_id: uuid.UUID,
-               status: MetricStatus,
-               importance: MetricImportance,
-               value: float,
-               failure_definition: DoubleFailureDefinition,
-               unit: Optional[str] = None,
-               should_display: Optional[bool] = None,
-               blocking: Optional[bool] = None):
-    self.id = uuid.UUID()
-    pass
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 parent_id: uuid.UUID,
+                 status: MetricStatus,
+                 importance: MetricImportance,
+                 value: float,
+                 failure_definition: DoubleFailureDefinition,
+                 unit: Optional[str] = None,
+                 should_display: Optional[bool] = None,
+                 blocking: Optional[bool] = None):
+        self.id = uuid.UUID()
+        pass
 
 @dataclass(init=False, repr=True)
 class DoubleOverTimeMetric(Metric):
-  doubles_over_time: MetricsData
-  statuses_over_time: MetricsData
+    doubles_over_time: MetricsData
+    statuses_over_time: MetricsData
 
-  failure_definition: DoubleFailureDefinition
+    failure_definition: DoubleFailureDefinition
 
-  start_time: Optional[Timestamp]
-  end_time: Optional[Timestamp]
+    start_time: Optional[Timestamp]
+    end_time: Optional[Timestamp]
 
-  y_axis_name: Optional[str]
-  legend_series_names: Optional[List[str]]
+    y_axis_name: Optional[str]
+    legend_series_names: Optional[List[str]]
 
-  def __init__(self, 
-               name: str,
-               description: str,
-               parent_id: uuid.UUID,
-               status: MetricStatus,
-               importance: MetricImportance,
-               doubles_over_time: MetricsData,
-               statues_over_time: MetricsData,
-               failure_definition: Optional[DoubleFailureDefinition] = None,
-               start_time: Optional[Timestamp] = None,
-               end_time: Optional[Timestamp] = None,
-               y_axis_name: Optional[str] = None,
-               legend_series_name: Optional[List[str]] = None,
-               blocking: Optional[bool] = None,
-               should_display: Optional[bool] = None):
-    self.id = uuid.UUID()
-    pass
+    def __init__(self, 
+                 name: str,
+                 description: str,
+                 parent_id: uuid.UUID,
+                 status: MetricStatus,
+                 importance: MetricImportance,
+                 doubles_over_time: MetricsData,
+                 statues_over_time: MetricsData,
+                 failure_definition: Optional[DoubleFailureDefinition] = None,
+                 start_time: Optional[Timestamp] = None,
+                 end_time: Optional[Timestamp] = None,
+                 y_axis_name: Optional[str] = None,
+                 legend_series_name: Optional[List[str]] = None,
+                 blocking: Optional[bool] = None,
+                 should_display: Optional[bool] = None):
+        self.id = uuid.UUID()
+        pass
 
 # Data representation
 
 @dataclass(init=False, repr=True)
 class MetricsData:
-  id: uuid.UUID
-  name: str
-  unit: Optional[str]
-  index_data: Optional[MetricsData]
+    id: uuid.UUID
+    name: str
+    unit: Optional[str]
+    index_data: Optional[MetricsData]
 
 
 @dataclass(init=False, repr=True)
 class SeriesMetricsData(MetricsData):
-  series: np.ndarray # normally, dtype='object'
+    series: np.ndarray # normally, dtype='object'
 
-  def group_by(self, SeriesMetricsData) -> GroupedMetricsData:
-    # also handles appropriate grouping of index.
-    pass
+    def group_by(self, SeriesMetricsData) -> GroupedMetricsData:
+        # also handles appropriate grouping of index.
+        pass
 
-  def __init__(self,
-               name: str,
-               series: np.ndarray,
-               unit: Optional[str] = None,
-               index_data: Optional[MetricsData] = None):
-    pass
+    def __init__(self,
+                 name: str,
+                 series: np.ndarray,
+                 unit: Optional[str] = None,
+                 index_data: Optional[MetricsData] = None):
+        pass
 
 @dataclass(init=False, repr=True)
 class GroupedMetricsData(MetricsData):
-  category_to_series: Dict[str, np.ndarray] # normally, dtype='object'
+    category_to_series: Dict[str, np.ndarray] # normally, dtype='object'
 
-  def __init__(self,
-               name: str,
-               category_to_series: Dict[str, np.ndarray],
-               unit: Optional[str] = None,
-               index_data: Optional[MetricsData] = None):
-    pass
+    def __init__(self,
+                 name: str,
+                 category_to_series: Dict[str, np.ndarray],
+                 unit: Optional[str] = None,
+                 index_data: Optional[MetricsData] = None):
+        pass
 
 print('Reached end of file.')
