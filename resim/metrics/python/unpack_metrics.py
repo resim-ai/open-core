0e884b6effb114e405aef974d0cc27ab2f891cf3
Michael Bauer <michael@resim.ai>
2023-11-10 20:32:24 +0000

    Add function signature

--- resim/metrics/python/unpack_metrics.py
+++ resim/metrics/python/unpack_metrics.py
@@ -5,5 +5,33 @@
 # https://opensource.org/licenses/MIT.
 
 
+"""This module contains a free function used to unpack a set of
+metrics and metrics data protos corresponding to a single job into a
+set of Metric and MetricsData objects referencing each other as
+appropriate.
 """
-This module contains a free function used to unpack
+
+import uuid
+from dataclasses import dataclass
+
+import resim.metrics.proto.metrics_pb2 as mp
+
+# Just a stub for now
+@dataclass
+class Metric:
+    ...
+
+@dataclass
+class MetricsData:
+    ...
+
+@dataclass
+class UnpackedMetrics:
+    metrics: dict[uuid.UUID, Metric]
+    metrics_data: dict[uuid.UUID, MetricsData]
+    names: set[str]
+
+
+def unpack_metrics(metrics: list[mp.Metric],
+                   metrics_data: list[mp.MetricsData]) -> UnpackedMetrics:
+    ...
