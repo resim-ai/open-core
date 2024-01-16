3755dc5fdfe30f041699725f04bd6f0d2d8a8ef2
Michael Bauer <michael@resim.ai>
2024-01-16 23:41:02 +0000

    Extra test

--- resim/math/clamp.hh
+++ resim/math/clamp.hh
@@ -4,6 +4,8 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+#include "resim/assert/assert.hh"
+
 namespace resim::math {
 
 template <typename Float>
