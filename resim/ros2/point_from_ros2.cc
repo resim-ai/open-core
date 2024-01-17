c99b6e34da2a3ea23512b579957724c8505a694c
knowlsie <tomobknowles@gmail.com>
2024-01-17 06:01:16 +0000

    Fix stuff

--- resim/ros2/point_from_ros2.cc
+++ resim/ros2/point_from_ros2.cc
@@ -1,21 +1,27 @@
 #include "resim/ros2/point_from_ros2.hh"
 
+#include "Eigen/Dense"
+#include "resim/transforms/frame.hh"
+#include "resim/transforms/proto/framed_vector_3_to_proto.hh"
+
 namespace resim::ros2 {
 
 // ROS2 converter for points
-resim::transforms::proto::FramedVector_3 convert_from_ros2(
+transforms::proto::FramedVector_3 convert_from_ros2(
     const geometry_msgs::msg::Point &ros2_msg) {
-  resim::transforms::proto::FramedVector_3 result;
+  transforms::proto::FramedVector_3 result;
+  constexpr int DIMS = 3;
 
-  result.add_algebra(ros2_msg.x);
-  result.add_algebra(ros2_msg.y);
-  result.add_algebra(ros2_msg.z);
+  const transforms::FramedVector<DIMS> vec(
+      Eigen::Vector3d{ros2_msg.x, ros2_msg.y, ros2_msg.z},
+      transforms::Frame<DIMS>::null_frame());
 
+  transforms::proto::pack(vec, &result);
   return result;
 }
 
 geometry_msgs::msg::Point convert_to_ros2(
-    const resim::transforms::proto::FramedVector_3 &resim_msg) {
+    const transforms::proto::FramedVector_3 &resim_msg) {
   geometry_msgs::msg::Point result;
 
   result.x = resim_msg.algebra(0);
