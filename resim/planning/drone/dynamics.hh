6ffa291fb97e884e9c21abb052cf0155a0bb4475
Michael Bauer <michael@resim.ai>
2024-05-02 06:02:24 +0000

    Add test

--- resim/planning/drone/dynamics.hh
+++ resim/planning/drone/dynamics.hh
@@ -1,3 +1,8 @@
+// Copyright 2024 ReSim, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 #pragma once
 
@@ -7,19 +12,21 @@
 
 namespace resim::planning::drone {
 
+// Dynamics for a simple quadcopter drone. These dynamics do not represent full
+// Newtonian dynamics but rather they allow the vertical acceleration due to
+// thrust and body angular accelration to be directly commanded as
+// controls. Inverse dynamics can be used to compute the torque and thrust force
+// required to attain these anyhow, and keeping these dynamics simple is
+// important for the performance of our planning algorithm.
 class Dynamics {
  public:
-  Dynamics() = default;
-  explicit Dynamics(
-      const double dt,
-      const double gravitational_acceleration_mpss)
-      : dt_{dt},
-        gravitational_acceleration_mpss_{gravitational_acceleration_mpss} {}
+  Dynamics(double dt, double gravitational_acceleration_mpss);
 
+  // The dynamics themselves.
   State operator()(
       const State &x,
       const Control &u,
-      NullableReference<DynamicsDiffs<State, Control>> diffs);
+      NullableReference<DynamicsDiffs<State, Control>> diffs) const;
 
  private:
   double dt_;
