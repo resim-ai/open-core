9fde9593143d11507ee4bc806085761640a60b19
Michael Bauer <michael@resim.ai>
2024-05-07 14:45:12 -0700

    Add a state type for mpc on drone motion (#123)
    ## Description of change
    Add a state type for model predictive control of drone motion including
    the drone's position, orientation, velocity, and angular velocity.
    
    ## Guide to reproduce test results.
    ```bash
    bazel test //resim/planning/drone:state_test
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in
    this PR.

---
+++ resim/planning/drone/state.hh
@@ -0,0 +1,42 @@
+// Copyright 2024 ReSim, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#pragma once
+
+#include <Eigen/Dense>
+
+#include "resim/math/vector_partition.hh"
+#include "resim/transforms/so3.hh"
+
+namespace resim::planning::drone {
+
+struct State {
+  enum BlockIndices : std::size_t {
+    ROTATION = 0,
+    POSITION,
+    ANGULAR_VELOCITY,
+    VELOCITY,
+    NUM_BLOCKS,
+  };
+
+  using Partition = resim::math::VectorPartition<3, 3, 3, 3>;
+
+  static constexpr int DIM = math::OffsetAt<Partition, NUM_BLOCKS>::value;
+  using Vec = Eigen::Matrix<double, DIM, 1>;
+
+  // We don't use SE3 directly for this because the cost functions get
+  // un-necessarily complicated to formulate in SE3.
+  transforms::SO3 scene_from_body_rotation;
+  Eigen::Vector3d position;
+  Eigen::Vector3d angular_velocity;
+  Eigen::Vector3d velocity;
+};
+
+State operator+(const State &x, const typename State::Vec &dx);
+
+typename State::Vec operator-(const State &x, const State &y);
+
+}  // namespace resim::planning::drone
