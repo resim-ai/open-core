729e88210319dda525468aa87c172ec7e9e0ecae
Michael Bauer <michael@resim.ai>
2024-05-24 20:43:38 +0000

    Make oci image for our simulator

---
+++ resim/simulator/sim_container_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# Run the simulation with the experience and return result.mcap:
+echo "Running the simulation..."
+./resim/simulator/resim_run -c /tmp/resim/inputs/experience.sim -l /tmp/resim/outputs/resim_log.mcap
+
+# Add visualization content
+echo "Adding Visualizations..."
+./resim/visualization/log/make_visualization_log -l /tmp/resim/outputs/resim_log.mcap -o /tmp/resim/outputs/vis.mcap
