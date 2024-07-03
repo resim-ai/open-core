13ea14315ac8279aafa0ea3db9fb6d4106b49168
Michael Bauer <michael@resim.ai>
2024-07-03 05:39:32 +0000

    copyright

--- resim/metrics/fetch_report_metrics.py
+++ resim/metrics/fetch_report_metrics.py
@@ -1,3 +1,10 @@
+# Copyright 2024 ReSim, Inc.
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+
 import asyncio
 import uuid
 
