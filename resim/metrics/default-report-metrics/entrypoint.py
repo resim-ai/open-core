9ec2d7c2999e1d1d3c25a84ce3c966b9319d0f02
Iain Whiteside <iain@resim.ai>
2024-07-10 16:13:39 +0000

    ss

--- resim/metrics/default-report-metrics/entrypoint.py
+++ resim/metrics/default-report-metrics/entrypoint.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 
-import json
 import pathlib
 import sys
 
@@ -9,12 +8,12 @@ from resim.metrics.default_report_metrics import main as default_report_metrics_
 REPORT_METRICS_CONFIG_PATH = "/tmp/resim/inputs/report_config.json"
 
 
-def main() -> None:
+async def main() -> None:
     report_metrics_config = pathlib.Path(REPORT_METRICS_CONFIG_PATH)
     is_report_metrics_mode = report_metrics_config.is_file()
     if is_report_metrics_mode:
         # Call the default_report_metrics main function:
-        default_report_metrics_main()
+        await default_report_metrics_main()
     else:
         # Otherwise, we can't do anything: exit 1
         print("Metrics Build only compatible with reports")
