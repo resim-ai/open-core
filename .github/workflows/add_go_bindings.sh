1dba247ed90114232c5cf0bf1938485f225dc15b
Michael Bauer <michael@resim.ai>
2024-07-27 00:15:03 +0000

    Start adding binding generation

---
+++ .github/workflows/add_go_bindings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# TODO(michael) Consider replacing this with some starklark code
+# rather than hard coding everyting
+
+if [[ -z $RELEASE_TAG ]]
+then
+    echo "RELASE_TAG must be defined"
+    exit 1
+fi
+
+BAZEL_BIN=$(bazel info bazel-bin)
+
+bazel build //resim/utils/proto:uuid_proto_go
+bazel build //resim/metrics/proto:metrics_proto_go
+
+ls ${BAZEL_BIN}/resim/utils/proto
+cp -r $BAZEL_BIN/resim/utils/proto/uuid_proto_go_/github.com/resim-ai/open-core/* .
+cp -r $BAZEL_BIN/resim/metrics/proto/metrics_proto_go_/github.com/resim-ai/open-core/* .
+
+git checkout -b "go/$RELEASE_TAG"
+git add uuid_proto
+git add metrics_proto
