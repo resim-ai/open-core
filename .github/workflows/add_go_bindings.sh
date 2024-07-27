a4c0e22f5f69b30500935e59d71744ece18865f9
Michael Bauer <michael@resim.ai>
2024-07-27 00:25:23 +0000

    Try it out

--- .github/workflows/add_go_bindings.sh
+++ .github/workflows/add_go_bindings.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-# TODO(michael) Consider replacing this with some starklark code
-# rather than hard coding everyting
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ -z $RELEASE_TAG ]]
+# TODO(michael) Consider replacing some of this with some starklark
+# code rather than hard coding everyting
+
+if [[ -z $RESIM_VERSION ]]
 then
     echo "RELASE_TAG must be defined"
     exit 1
@@ -19,7 +21,10 @@ ls ${BAZEL_BIN}/resim/utils/proto
 cp -r $BAZEL_BIN/resim/utils/proto/uuid_proto_go_/github.com/resim-ai/open-core/* .
 cp -r $BAZEL_BIN/resim/metrics/proto/metrics_proto_go_/github.com/resim-ai/open-core/* .
 
-git checkout -b "go/$RELEASE_TAG"
+git checkout -b "go/$RESIM_VERSION-branch"
 git add uuid_proto
 git add metrics_proto
-git commit -m "go/$RELEASE_TAG"
+git commit -m "go/$RESIM_VERSION-branch"
+git push --force origin
+
+git checkout $CURRENT_BRANCH
