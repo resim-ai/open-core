0288af44506a892e3b17c826466fab490b225afc
Michael Bauer <michael@resim.ai>
2024-08-07 19:54:51 +0000

    Add in missing checks again

--- .github/workflows/add_go_bindings.sh
+++ .github/workflows/add_go_bindings.sh
@@ -22,11 +22,11 @@ ls ${BAZEL_BIN}/resim/utils/proto
 cp -r $BAZEL_BIN/resim/utils/proto/uuid_proto_go_/github.com/resim-ai/open-core/* .
 cp -r $BAZEL_BIN/resim/metrics/proto/metrics_proto_go_/github.com/resim-ai/open-core/* .
 
-git checkout -b "go/$RESIM_VERSION-branch"
+git checkout -b "$RESIM_VERSION-go-branch"
 git add uuid_proto
 git add metrics_proto
-git commit -m "Generate go bindings for go/$RESIM_VERSION"
-git tag "go/$RESIM_VERSION"
-git push origin tag "go/$RESIM_VERSION"
+git commit -m "Generate go bindings for $RESIM_VERSION-go"
+git tag "$RESIM_VERSION-go"
+git push origin tag "$RESIM_VERSION-go"
 
 git checkout $CURRENT_BRANCH
