9b6ff83b3631e111eefe98c26445b15f5621e8cc
Michael Bauer <michael@resim.ai>
2024-08-07 20:00:39 +0000

    Update go tag in check_tags_available

--- .github/workflows/check_tags_available.sh
+++ .github/workflows/check_tags_available.sh
@@ -6,7 +6,7 @@ git remote -v
 ls -ltha .
 
 MAIN_RELEASE_TAG=$(git ls-remote origin refs/tags/$RESIM_VERSION)
-GO_RELEASE_TAG=$(git ls-remote origin refs/tags/go/$RESIM_VERSION)
+GO_RELEASE_TAG=$(git ls-remote origin refs/tags/$RESIM_VERSION-go)
 
 if [[ -n "${MAIN_RELEASE_TAG}" || -n "${GO_RELEASE_TAG}" ]]; then
     echo "Tag collision! Can't release on this tag!"
