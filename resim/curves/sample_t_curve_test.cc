e88ea1b8e5d2a705df9bdcaa74c7d63f3368e087
Michael Bauer <michael@resim.ai>
2025-02-26 22:38:34 +0000

    copyright

--- resim/curves/sample_t_curve_test.cc
+++ resim/curves/sample_t_curve_test.cc
@@ -1,3 +1,8 @@
+// Copyright 2025 ReSim, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 #include "resim/curves/sample_t_curve.hh"
 
