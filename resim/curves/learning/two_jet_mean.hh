3b597a38e8c7b184fc1807b326b19f17c844f397
Michael Bauer <michael@resim.ai>
2025-03-03 21:37:40 +0000

    Comments

--- resim/curves/learning/two_jet_mean.hh
+++ resim/curves/learning/two_jet_mean.hh
@@ -14,6 +14,20 @@
 
 namespace resim::curves::learning {
 
+// Compute the mean of a set of two jets. Since two jets exist on a manifold,
+// this function computes the two jet u such that sum_i(x_i (-) u) = 0 where (-)
+// is defined as the left minus operator as defined in
+// resim/curves/optimization/two_jet_tangent_space.hh. Another way of saying
+// this is that u is the mean of all the samples when they're expressed in u's
+// tangent space. We use iteration to find u  and therefore a tolerance and a
+// maximum number of iterations must be provided.
+// @param[in] samples - The samples to compute the mean of.
+// @param[in] tolerance - The absolute tolerance for computing the mean.
+//                        Iteration terminates once updates get smaller in L2
+//                        norm than this.
+// @param[in] max_iterations - The maximum number of iterations allowed before
+//                             terminating unsuccessfully.
+// @returns The mean as described above, or a bad status otherwise.
 StatusValue<TwoJetL<resim::transforms::SE3>> two_jet_mean(
     const std::span<const TwoJetL<resim::transforms::SE3>> &samples,
     double tolerance,
